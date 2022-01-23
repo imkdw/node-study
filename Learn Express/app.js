24dcde0d5d232926df774dad22a1b85156fdd939
Dongwoo-Kim <dongeu47@gmail.com>
2022-01-23 14:02:01 +0900

    220123

--- Learn Express/app.js
+++ Learn Express/app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
+nunjucks.configure('views', {
+  express: app,
+  watch: true
+})
 
-app.use('/', indexRouter);
-app.use('/user', userRouter);
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  res.status(404).send('Not Found');
-});
+app.get('/', (req, res) => {
+  res.locals.title = 'This is Title';
+  res.render('index.html');
+})
 
 app.listen(app.get('port'), () => console.log(`${app.get('port')}번 에서 서버 실행중`));
---
+++ Learn sequelize/models/comment.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Comment extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      comment: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      modelName: 'Comment',
+      tableName: 'comments',
+      paranoid: false,
+      charset: 'utf8',
+      collate: 'utf8_general_ci'
+    });
+  }
+
+  static associations(db) { }
+};
