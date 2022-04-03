0a1bd4be9b6508e221b2680cb89aec65a28bd5b4
Dongwoo-Kim <dongeu47@gmail.com>
2022-04-03 14:42:28 +0900

    220403
    노드의 미들웨어 사용법 - dotenv, moran 등 사용해보기

--- learn-express/app.ts
+++ learn-express/app.ts
@@ -1,11 +1,34 @@
 import express, { Request, Response } from "express";
 import path from "path";
 
-const app = express();
+/**
+ * morgan : req, res에 대한 로그를 출력해주는 모듈. dev, combined, common 등 의 옵션이 있다
+ * dotenv : 디렉토리 내 .env 파일을 읽어서 process.env 으로 만들어 주는 모듈
+ */
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import dotenv from 'dotenv'
 
-// 서버가 실행될 포트를 지정
-// process.env에 포트가 없다면 기본값은 3000번
+dotenv.config()
+const app = express();
 app.set("port", process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_PARSER,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  },
+  name: 'session-cookie'
+}))
 
 app.use((req: Request, res: Response, next: any) => {
   console.log("모든 요청에 대해서 실행");
