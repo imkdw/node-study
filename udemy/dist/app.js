63ff05390b63c6f02a666df3f63b2436e109ce81
Dongwoo-Kim <dongeu47@gmail.com>
2022-08-12 20:57:34 +0900

    220812
    Express 공부중

--- udemy/dist/app.js
+++ udemy/dist/app.js
@@ -3,9 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-var http_1 = __importDefault(require("http"));
 var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = (0, express_1["default"])();
-var server = http_1["default"].createServer(app);
-server.listen(3000);
+app.use(body_parser_1["default"].urlencoded({ extended: false }));
+app.use("/app-product", function (req, res, next) {
+    res.send("<form action=\"/product\" method=\"POST\"><input type=\"text\" name=\"title\"><button type=\"submit\">Add Product</button></form>");
+});
+app.post("/product", function (req, res, next) {
+    console.log(JSON.parse(JSON.stringify(req.body)));
+    res.redirect("/");
+});
+app.use("/", function (req, res, next) {
+    res.send("<h1>Hello Express!</h1><br><a href='/app-product'>app-product</a>");
+});
+app.listen(3000);
 //# sourceMappingURL=app.js.map
--- udemy/src/app.ts
+++ udemy/src/app.ts
@@ -1,9 +1,23 @@
-import http from "http";
-
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use("/app-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(JSON.parse(JSON.stringify(req.body)));
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Express!</h1><br><a href='/app-product'>app-product</a>");
+});
+
+app.listen(3000);
