1568b09847e80434c86fd5d94e11b700b7aef9b8
Dongwoo-Kim <dongeu47@gmail.com>
2022-08-26 23:20:22 +0900

    220826
    api

--- lostark-api/dist/app.js
+++ lostark-api/dist/app.js
@@ -1,23 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var morgan_1 = __importDefault(require("morgan"));
-var stat_1 = __importDefault(require("./routes/stat"));
-var app = (0, express_1["default"])();
-app.set("port", 3000);
-/** Set Middleware */
-app.use((0, morgan_1["default"])("dev"));
-app.use(express_1["default"].urlencoded({ extended: true }));
-app.use(express_1["default"].json());
-/** Set Router */
-app.use("/stat", stat_1["default"]);
-app.get("/", function (req, res) {
-    res.send("noobzz");
-});
-app.listen(app.get("port"), function () {
-    console.log("Server on", app.get("port"));
-});
+// import express from "express";
+// import morgan from "morgan";
+// import StatRouter from "./routes/stat";
+// const app = express();
+// app.set("port", 3000);
+// /** Set Middleware */
+// app.use(morgan("dev"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// /** Set Router */
+// app.use("/stat", StatRouter);
+// app.get("/", (req, res) => {
+//   res.send("noobzz");
+// });
+// app.listen(app.get("port"), () => {
+//   console.log("Server on", app.get("port"));
+// });
+var a = 3;
+console.log(~a);
 //# sourceMappingURL=app.js.map
--- lostark-api/src/app.ts
+++ lostark-api/src/app.ts
@@ -1,22 +1,25 @@
-import express from "express";
-import morgan from "morgan";
-import StatRouter from "./routes/stat";
+// import express from "express";
+// import morgan from "morgan";
+// import StatRouter from "./routes/stat";
 
-const app = express();
-app.set("port", 3000);
+// const app = express();
+// app.set("port", 3000);
 
-/** Set Middleware */
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// /** Set Middleware */
+// app.use(morgan("dev"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-/** Set Router */
-app.use("/stat", StatRouter);
+// /** Set Router */
+// app.use("/stat", StatRouter);
 
-app.get("/", (req, res) => {
-  res.send("noobzz");
-});
+// app.get("/", (req, res) => {
+//   res.send("noobzz");
+// });
 
-app.listen(app.get("port"), () => {
-  console.log("Server on", app.get("port"));
-});
+// app.listen(app.get("port"), () => {
+//   console.log("Server on", app.get("port"));
+// });
+
+let a = 3;
+console.log(~a);
