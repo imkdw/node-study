c3c7c47c22dfb19d143c69c921e958cab3a00b7b
Dongwoo-Kim <dongeu47@gmail.com>
2022-10-25 20:29:53 +0900

    221025
    221025

--- multer/dist/app.js
+++ multer/dist/app.js
@@ -4,19 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+// import fileUpload from "express-fileupload";
 var multer_1 = __importDefault(require("multer"));
+var morgan_1 = __importDefault(require("morgan"));
+var uuid_1 = require("uuid");
+var cors_1 = __importDefault(require("cors"));
 var app = (0, express_1["default"])();
 app.set("port", 5000);
 var storage = multer_1["default"].diskStorage({
     destination: function (req, file, cb) {
         cb(null, "images");
+    },
+    filename: function (req, file, cb) {
+        cb(null, (0, uuid_1.v4)() + "-" + file.originalname);
     }
 });
+app.use((0, cors_1["default"])());
 app.use(express_1["default"].urlencoded({ extended: true }));
 app.use(express_1["default"].json());
-app.use((0, multer_1["default"])({ storage: storage }).single("image"));
+app.use((0, morgan_1["default"])("dev"));
+app.use((0, multer_1["default"])({ storage: storage }).array("image"));
 app.post("/images", function (req, res, next) {
-    console.log(req.file);
+    console.log(req.files);
+    res.json("Hello");
 });
 app.listen(app.get("port"), function () {
     console.log("port:", app.get("port"));
--- multer/src/app.ts
+++ multer/src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
+// import fileUpload from "express-fileupload";
 import multer from "multer";
+import morgan from "morgan";
+import { v4 } from "uuid";
+import cors from "cors";
 
 const app = express();
 app.set("port", 5000);
@@ -8,14 +12,20 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
   },
+  filename: (req, file, cb) => {
+    cb(null, v4() + "-" + file.originalname);
+  },
 });
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(multer({ storage }).single("image"));
+app.use(morgan("dev"));
+app.use(multer({ storage }).array("image"));
 
 app.post("/images", (req, res, next) => {
-  console.log(req.file);
+  console.log(req.files);
+  res.json("Hello");
 });
 
 app.listen(app.get("port"), () => {
